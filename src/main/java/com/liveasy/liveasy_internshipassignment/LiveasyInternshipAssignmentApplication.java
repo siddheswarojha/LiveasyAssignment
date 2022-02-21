c54328b709ad6660e077d490281b581cfdcc52af
siddheswarojha <siddheswarojha23@gmail.com>
2022-02-22 00:48:41 +0530

    Initial backend Setup

---
+++ src/main/java/com/liveasy/liveasy_internshipassignment/LiveasyInternshipAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.liveasy.liveasy_internshipassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyInternshipAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveasyInternshipAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liveasy/liveasy_internshipassignment/LiveasyInternshipAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveasy.liveasy_internshipassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveasyInternshipAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
